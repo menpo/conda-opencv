44e2a8ab57a02e29fc87753b7191e7a80cbcfb1a
Patrick Snape <patricksnape@gmail.com>
2015-08-19 21:43:38 +0100

    Fix lib CMAKE issue on OSX
    CMake 3.x adds lib for some reason, to the binaries/libraries
    and this causes conda to be unable to relocate. Therefore,
    just move them myself before conda does it.

--- conda/build.sh
+++ conda/build.sh
@@ -11,8 +11,10 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   DYNAMIC_EXT="so"
   TBB=""
   OPENMP="-DWITH_OPENMP=1"
+  IS_OSX=0
 fi
 if [ "$(uname -s)" == "Darwin" ]; then
+  IS_OSX=1
   DYNAMIC_EXT="dylib"
   OPENMP=""
   TBB="-DWITH_TBB=1 -DTBB_LIB_DIR=$LIBRARY_PATH -DTBB_INCLUDE_DIRS=$INCLUDE_PATH -DTBB_STDDEF_PATH=$INCLUDE_PATH/tbb/tbb_stddef.h"
@@ -46,4 +48,41 @@ cmake .. -G"$CMAKE_GENERATOR"                                            \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j${CPU_COUNT}
 make install
+
+if [ $IS_OSX -eq 1 ]; then
+    # Fix the lib prefix for the libraries
+    # Borrowed from
+    # http://answers.opencv.org/question/4134/cmake-install_name_tool-absolute-path-for-library-on-mac-osx/
+    CV_LIB_PATH=$PREFIX/lib
+    find ${CV_LIB_PATH} -type f -name "libopencv*.dylib" -print0 | while IFS="" read -r -d "" dylibpath; do
+        echo install_name_tool -id "$dylibpath" "$dylibpath"
+        install_name_tool -id "$dylibpath" "$dylibpath"
+        otool -L $dylibpath | grep libopencv | tr -d ':' | while read -a libs ; do
+            [ "${file}" != "${libs[0]}" ] && install_name_tool -change ${libs[0]} `basename ${libs[0]}` $dylibpath
+        done
+    done
+
+    # Fix the lib prefix for the cv2.so
+    # Borrowed from
+    # http://answers.opencv.org/question/4134/cmake-install_name_tool-absolute-path-for-library-on-mac-osx/
+    find ${SP_DIR} -type f -name "cv2.so" -print0 | while IFS="" read -r -d "" dylibpath; do
+    echo install_name_tool -id "$dylibpath" "$dylibpath"
+        install_name_tool -id "$dylibpath" "$dylibpath"
+        otool -L $dylibpath | grep libopencv | tr -d ':' | while read -a libs ; do
+            [ "${file}" != "${libs[0]}" ] && install_name_tool -change ${libs[0]} `basename ${libs[0]}` $dylibpath
+        done
+    done
+
+    # Fix the lib prefix for the binaries
+    # Borrowed from
+    # http://answers.opencv.org/question/4134/cmake-install_name_tool-absolute-path-for-library-on-mac-osx/
+    CV_BIN_PATH=$PREFIX/bin
+    find ${CV_BIN_PATH} -type f -name "opencv*" -print0 | while IFS="" read -r -d "" dylibpath; do
+        echo install_name_tool -id "$dylibpath" "$dylibpath"
+        install_name_tool -id "$dylibpath" "$dylibpath"
+        otool -L $dylibpath | grep libopencv | tr -d ':' | while read -a libs ; do
+            [ "${file}" != "${libs[0]}" ] && install_name_tool -change ${libs[0]} `basename ${libs[0]}` $dylibpath
+        done
+    done
+fi
 
